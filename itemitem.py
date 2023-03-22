9e464e8deda1dee34fdea6d4ef509413134643f0
Kevin Prom <promkev@gmail.com>
2023-03-22 18:44:11 -0400

    Implement Pearson correlation for item-item filtering

--- itemitem.py
+++ itemitem.py
@@ -1,21 +1,35 @@
 # %%
 from pyspark.sql.functions import *
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType
 
-# %%
-
-# Create a SparkSession
+# %% Create a SparkSession
 spark = SparkSession.builder.appName("GameMatrix").getOrCreate()
 
-# %%
-# Create a sample DataFrame with some data
-data = [("user1", "game1", 10), ("user2", "game2", 20), ("user1", "game2", 15), ("user2", "game1", 5)]
+# %% Sample data
+data = [("user1", "game1", 10), ("user2", "game2", 20), ("user1", "game2", 15), ("user2", "game1", 5), 
+        ("user1", "game3", 30), ("user2", "game3", 25), ("user3", "game3", 15), ("user3", "game1", 10)]
 df = spark.createDataFrame(data, ["steamid", "appid", "playtime_forever"])
 
-# %%
-# Use groupBy and pivot to create the matrix
+# %% Create the game matrix
 game_matrix = df.groupBy("appid").pivot("steamid").agg(first("playtime_forever"))
-
-# %%
-# Show the resulting matrix
 game_matrix.show()
+
+# %% Replace null values with 0
+for col in game_matrix.columns[1:]:
+    game_matrix = game_matrix.withColumn(col, game_matrix[col].cast(DoubleType()))
+    game_matrix = game_matrix.fillna(0.0, subset=[col])
+
+# %% Extract columns with numerical values and assemble them into a vector column
+numeric_cols = game_matrix.columns[1:]
+assembler = VectorAssembler(inputCols=numeric_cols, outputCol="features")
+vector_matrix = assembler.transform(game_matrix).select("features")
+
+# %% Compute the Pearson correlation matrix between the rows
+pearson_matrix = Correlation.corr(vector_matrix, "features", "pearson")
+
+# %% Print the correlation array (numpy)
+corr_array = pearson_matrix.head()[0].toArray()
+print(corr_array)
